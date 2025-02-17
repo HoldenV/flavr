023a65c56c56c687a2a9e0f1a2a28e809a1ba53d
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2025-02-17 15:58:30 -0600

    card swiper demo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,16 +4,46 @@ Description: This is where we define the widgets of the home screen
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> imagePaths = [
+      'lib/misc/example_images/image1.png',
+      'lib/misc/example_images/image2.png',
+      'lib/misc/example_images/image3.png',
+    ];
+
+    List<Widget> cards = imagePaths.map((path) {
+      return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        elevation: 4,
+        child: Center(
+          child: SizedBox(
+            width: 300,
+            height: 400,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Image.asset(path, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      );
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: const Center(
-        child: Text('Welcome to the Home Screen!'),
+      body: Center(
+        child: CardSwiper(
+          cardsCount: cards.length,
+          cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+              cards[index],
+        ),
       ),
     );
   }
