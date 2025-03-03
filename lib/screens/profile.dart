50adb661d570ba7ac9b93cba86b4681c46938eab
Katharine Swann <katharinesswann@gmail.com>
2025-03-03 01:05:18 -0600

    setting up profile page
    adding in basic sectioning and default image

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+//code for the profile page on the UI
+
+import 'package:flutter/material.dart'; //import statement for Flutter material package
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,11 +9,49 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile Screen'),
+        title: const Text('Flavr Profile'),
       ),
-      body: const Center(
-        child: Text('Change your profile and settings here!'),
-      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(15.0), //15 pixels
+        child: Column(
+          children: [
+            const Center(
+              child: CircleAvatar( //circle for profile image to fill
+                radius: 70,
+                backgroundImage: AssetImage('lib/assets/default_profile.png'),
+                //not loading correctly, try new path --> update pubspec.yaml assets to fix
+              )
+            ),
+            
+            const SizedBox(height: 10), //space between pic and text for name
+            const Center(
+              child: Text(
+                'John Doe',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            const Text(
+              'User Information',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            const Divider( //line to divide up sections
+              color: Colors.black, thickness: 2,
+            ),
+
+            const SizedBox(height: 10),
+            const Text(
+              'Preferences',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            const Divider( //line to divide up sections
+              color: Colors.black, thickness: 2,
+            ),
+          ]
+        ),
+      )
     );
   }
 }
