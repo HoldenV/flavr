b5cd3b038019dd74f8c792177a42bfb008a942c5
Charles Horner <c710h797@ku.edu>
2025-03-08 23:00:17 -0600

    fake_data_toy

--- backend/cbe.py
+++ backend/cbe.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 
-def cbe(user_taste_vector):
-    dish_matrix = pd.read_csv('dish_metadata.csv')
+def cbe(user_taste_vector, fake_data=False):
+    if fake_data:
+        dish_matrix = pd.read_csv('../dish_metadata.csv')
+    else:
+        dish_matrix = pd.read_csv('dish_metadata.csv')
     dish_matrix.set_index('dish name', inplace=True)
 
     dish_matrix = dish_matrix.loc[user_taste_vector.index]
 
     tastes = dish_matrix.T.dot(user_taste_vector)
     rankings = dish_matrix.dot(tastes)
-    print(rankings)
+    rankings = rankings.squeeze()
+    print(rankings.sort_values(ascending=False))
 
+if __name__ == "__main__":
+    user_taste_vector = pd.read_csv('fake_data/data/user_taste_vector.csv', index_col='dish')
+    cbe(user_taste_vector)
--- backend/fake_data/make_fake_data.py
+++ backend/fake_data/make_fake_data.py
@@ -9,6 +9,8 @@ sys.path.append(parent_dir)
 
 # Import the user_taste_vector function
 from user_taste_vector import get_user_taste_vector
+from cbe import cbe
+
 
 all_dishes = ['pizza', 'flatbread pizza', 'burrito', 'steak', 'mac and cheese', 'chicken tikka masala', "general tso's chicken", 'chili', 'lasagna', 'pasta carbonara', 'fettuccine alfredo', 'calzone', 'gnocchi', 'saltimbocca', 'coq au vin', 'bouillabaisse', 'duck confit', 'cassoulet', 'quiche lorraine', 'sole meunière', 'tartiflette', 'street tacos', 'mole poblano', 'enchiladas', 'pozole', 'tamales', 'cochinita pibil', 'birria', 'carne asada', 'sushi', 'ramen', 'shrimp tempura', 'tonkatsu', 'yakitori', 'udon', 'okonomiyaki', 'shabu-shabu', 'donburi', 'shawarma', 'kofta', 'tabbouleh', 'shakshuka', 'falafel', 'mansaf', 'kibbeh', 'moussaka', 'souvlaki', 'dolma (stuffed grapeleaves)', 'spanakopita', 'gyros', 'stifado', 'fasolada', 'kleftiko', 'peking duck', 'kung pao chicken', 'mapo tofu', 'sweet and sour pork', 'hot pot', 'chow mein', 'zongzi', 'beef and broccoli', 'char siu', 'butter chicken', 'biryani', 'rogan josh', 'paneer tikka', 'chana masala', 'tandoori chicken', 'vindaloo', 'pad thai', 'green curry', 'massaman curry', 'khao soi', 'larb', 'tom kha gai', 'paella', 'tortilla española', 'fabada', 'cochinillo', 'bacalao a la vizcaína', 'bibimbap', 'kimchi jjigae', 'bulgogi', 'samgyeopsal', 'sundubu jjigae', 'galbi', 'naengmyeon', 'dakgalbi', "shepard's pie", 'fish and chips', 'poke', 'huli huli', 'fajita', 'quesadilla', 'bbq ribs', 'buffalo wings', 'pot roast', 'jambalaya', 'alaskan salmon', 'fried chicken', 'chicken pot pie', 'chicken nuggets or tenders', 'po boy', 'cobb salad', 'caesar salad', 'italian salad', 'taco salad', 'greek salad', 'fattoush', 'wedge salad', 'pasta salad', 'reuben sandwich', 'club sandwich', 'philly cheese steak', 'fried chicken sandwich', 'pulled pork sandwich', 'panini/grilled cheese w/ tomato soup', 'blt', 'french dip', 'italian sandwich', 'sloppy joe', 'meatball sub', 'chicken noodle soup', 'pho', 'minestrone', 'french onion soup', 'clam chowder', 'cream of mushroom soup', 'lentil soup', 'broccoli and cheddar soup', 'tortilla soup']
 
@@ -62,3 +64,6 @@ with open('data/swipes.pickle', 'wb') as f:
 print("Making user taste vector from history and swipes...")
 user_taste_vector = get_user_taste_vector(swipes, history)
 user_taste_vector.to_csv('data/user_taste_vector.csv')
+
+print("Running CBE...")
+cbe(user_taste_vector, fake_data=True)
