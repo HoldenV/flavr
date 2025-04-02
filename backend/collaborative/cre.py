4fa63af42be935d48c29c7066d858304c5a042e3
jspenjs17 <jspencer.js17@gmail.com>
2025-04-02 14:38:19 -0500

    CRE driver code

--- backend/collaborative/cre.py
+++ backend/collaborative/cre.py
@@ -1,11 +1,47 @@
 import pandas as pd
-import json
+from sklearn.metrics.pairwise import cosine_similarity
+from similar_users_matrix import make_similar_users_matrix
 
-def cre(similar_users_matrix, user_taste_vector):
-    pass
+# This function needs to be fast
+def cre(similar_users_matrix, user_taste_vector, user_id):
+    # Align the user_taste_vector with the columns of similar_users_matrix
+    similar_users_matrix.loc[user_id] = user_taste_vector.squeeze()
+
+    # Calculate User Similarity using cosine similarity
+    user_similarity = cosine_similarity(similar_users_matrix)
+    user_similarity_df = pd.DataFrame(user_similarity, 
+                                      index=similar_users_matrix.index, 
+                                      columns=similar_users_matrix.index)
+
+    # find most similar users to our guy
+    user_ratings = similar_users_matrix.loc[user_id].dropna()
+    similarity_scores = user_similarity_df.loc[user_id]
+    similarity_scores = similarity_scores[similarity_scores > 0]
+    similarity_scores = similarity_scores.sort_values(ascending=False)
+    similar_users = similarity_scores.index
+    recommendations_df = pd.DataFrame()
+
+    # put those in a df
+    for user in similar_users:
+        user_ratings = similar_users_matrix.loc[user].dropna()
+        recommendations_df = recommendations_df._append(user_ratings)
+
+    # average similar user's tastes to get our guy's recommendations
+    recommended_dishes = recommendations_df.mean(axis=0)
+    recommended_dishes = recommended_dishes.sort_values(ascending=False) 
+
+    return recommended_dishes
+
 
 def main():
-    pass
+    # load pre-made data
+    SMU = make_similar_users_matrix("survey_responses.json")
+    SMU = SMU.drop(columns=['timestamp'])
+    UTV = pd.read_csv('user_taste_vector.csv', index_col='dish')
+
+    # print result of CRE
+    print(cre(SMU, UTV, 0))
+
 
 if __name__ == "__main__":
     main()
--- backend/collaborative/similar_users_matrix.py
+++ backend/collaborative/similar_users_matrix.py
@@ -8,10 +8,11 @@ def make_similar_users_matrix(json_name, download=False):
 
     # load data, survey_responses.json could be loaded dynamically from firestore
     with open(json_name) as f:
-        json_data = json_data = json.load(f)
-
+        json_data = json.load(f)
+        
     df = pd.DataFrame(json_data)
-    df.set_index('User_number', inplace=True)
+    df.columns = df.columns.str.lower()
+    df.set_index('user_number', inplace=True)
 
     # do some pandas magic, convert "Looks good" to 1, "" to 0, and "Doesn't look good" to -1
     df.replace({"Looks good": 1, "": 0, "Doesn't look good": -1}, inplace=True)
