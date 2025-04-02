a44eafc3a181941a7b99226075e97204513a9ecf
jspenjs17 <jspencer.js17@gmail.com>
2025-04-02 13:09:48 -0500

    similar_users_matrix.py complete and start cre.py

---
+++ backend/collaborative/cre.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import json
+
+def cre(similar_users_matrix, user_taste_vector):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- backend/collaborative/similar_users_matrix.py
+++ backend/collaborative/similar_users_matrix.py
@@ -1,12 +1,25 @@
-import numpy as np
 import pandas as pd
 import json
 
-def make_similar_users_matrix(user_data):
-    pass
+def make_similar_users_matrix(json_name, download=False):
+    # support "file_name" and "file_name.json"
+    if json_name[-5:] != ".json":
+        json_name += ".json"
+
+    # load data, survey_responses.json could be loaded dynamically from firestore
+    with open(json_name) as f:
+        json_data = json_data = json.load(f)
+
+    df = pd.DataFrame(json_data)
+    df.set_index('User_number', inplace=True)
+
+    # do some pandas magic, convert "Looks good" to 1, "" to 0, and "Doesn't look good" to -1
+    df.replace({"Looks good": 1, "": 0, "Doesn't look good": -1}, inplace=True)
+
+    return df
 
 def main():
-    pass
+    print(make_similar_users_matrix("survey_responses.json"))
 
 if __name__ == "__main__":
     main()
