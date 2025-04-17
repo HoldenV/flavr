77ce6a9837ec1080aa83986426f17c3a84660312
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2025-04-17 10:53:41 -0500

    simple splash screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// not in use. not tested. should be same gradient as home screen but
+// with icon and progress indicator
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, Colors.orange],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/assets/app_icon.png',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(height: 20),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
